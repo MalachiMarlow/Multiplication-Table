06bc81bc2f4b5d4d3f9df9cb741073bc5fda1dc0
Tusk <Tuskman045@gmail.com>
2025-02-05 08:09:15 -0700

    Add project files.

---
+++ Multiplication Table/Multiplication_Table.vb
@@ -0,0 +1,16 @@
+﻿Option Compare Text
+Option Explicit On
+Option Strict On
+
+Module Multiplication_Table
+
+    Sub Main()
+        For i As Integer 1 To 10 Step 1
+            Console.WriteLine(i)
+            Console.WriteLine()
+
+
+
+    End Sub
+
+End Module
---
+++ Multiplication Table/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Multiplication Table")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Idaho State University")>
+<Assembly: AssemblyProduct("Multiplication Table")>
+<Assembly: AssemblyCopyright("Copyright © Idaho State University 2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("45beff85-b296-4536-8fe2-f798ece1c34c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
